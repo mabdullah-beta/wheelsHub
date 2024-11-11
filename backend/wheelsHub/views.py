a63ee47fff92883a1313636c6b1e466680cbe3dc
Moiz Husnain <moiz@grandeur.tech>
2024-11-11 21:25:42 +0500

    added search

--- backend/wheelsHub/views.py
+++ backend/wheelsHub/views.py
@@ -1,6 +1,7 @@
 # Libs
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 
 
 # Import db models 
@@ -13,11 +14,64 @@ from .serializers import DealSerializer
 @api_view(['GET'])
 def deals(request):
 
+    # Get query parameters
+    title = request.GET.get('title', '')
+    make = request.GET.get('make', '')
+    year = request.GET.get('year')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    transmission = request.GET.get('transmission')
+    location = request.GET.get('location', '')
+
+    # Initialize query
+    query = Q()
+
+    # Filter based on title
+    if title:
+
+        query &= Q(title__icontains=title)
+        
+    # Filter based on make
+    if make:
+
+        query &= Q(make__icontains=make)
+        
+    # Filter based on year
+    if year:
+
+        query &= Q(year=year)
+        
+    # Filter based on price range
+    if price_min and price_max:
+
+        query &= Q(price__gte=price_min) & Q(price__lte=price_max)
+
+    elif price_min:
+
+        query &= Q(price__gte=price_min)
+
+    elif price_max:
+
+        query &= Q(price__lte=price_max)
+        
+    # Filter based on transmission
+    if transmission:
+
+        query &= Q(transmission=transmission)
+        
+    # Filter based on location
+    if location:
+
+        query &= Q(location__icontains=location)
+
     # Request server to get all deals
-    deals = Deal.objects.all().values()
+    deals = Deal.objects.filter(query)
+
+    # Serialize data because it needs to converted from python object to json
+    serializer = DealSerializer(deals, many=True)
 
     # Return json response
-    return Response({ "success": True, "deals": list(deals) })
+    return Response({ "success": True, "deals": serializer.data })
 
 # Get deal by id
 @api_view(['GET'])
